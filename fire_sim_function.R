d59c54c5baf49e1d6da73c375b8f7706c8810c61
Andrew Becker <andrewbecker93@gmail.com>
2018-12-02 12:22:49 -0500

    Split fire_sim into fire_sim and growth_sim

--- fire_sim_function.R
+++ fire_sim_function.R
@@ -2,7 +2,45 @@
 
 library(simecol)
 
-fire_sim  <- function(init, params) {
+#fire_sim  <- function(init, params) {
+#  p_treeburn <- params[1]
+#  p_uburn <- params[2]
+#  p_burnout_u <- params[3]
+#  p_stayburn_u <- params[4]
+#  p_burnout_t <- params[5]
+#  p_seed <- params[6]
+#  p_spread <- params[7]
+#  p_grow <- params[8]
+#  x   <- init
+#  n   <- nrow(x)
+#  m   <- ncol(x)
+#  wdist <- matrix(c(0,1,0,1,0,1,0,1,0), ncol=3)
+#  trees  <- simecol::neighbors(init, state=1, wdist=wdist)
+#  utrees <- simecol::neighbors(init, state=2, wdist=wdist)
+#  t_fires <- simecol::neighbors(init, state=3, wdist=wdist)
+#  u_fires <- simecol::neighbors(init, state=4, wdist=wdist)
+#  rand_mat <- runif(init)
+#  if(any(x>=3)){
+#    ## burning rules
+#    x[which(init==1 & t_fires>0 & rand_mat < (1-p_treeburn^t_fires))] <- 3
+#    x[which(init==2 & (t_fires>0 | u_fires>0) & rand_mat < p_uburn)] <- 4
+#    ## burnout rules
+#    x[which(init==4 & rand_mat < p_burnout_u)] <- 1
+#    x[which(init==4 & rand_mat >= (p_burnout_u+p_stayburn_u))] <- 3
+#    x[which(init==3& rand_mat <p_burnout_t) ] <- 0
+#  }else{
+#    ## lightning rule
+#    x[which(init==2 & rand_mat<(3/n^2))] <- 4
+#    ## growth of trees and understory growth
+#    x[which(init==0 & rand_mat < p_seed)] <- 1
+#    x[which(init==1 & rand_mat < p_grow)] <- 2
+#   x[which(init==0 & (trees>0 |utrees>0) & rand_mat < p_spread*(trees+utrees))] <- 1
+#  }
+#  return(x)
+#}
+
+fire_sim <- function(map, params){
+  #run fire until burnt out
   p_treeburn <- params[1]
   p_uburn <- params[2]
   p_burnout_u <- params[3]
@@ -11,16 +49,13 @@ fire_sim  <- function(init, params) {
   p_seed <- params[6]
   p_spread <- params[7]
   p_grow <- params[8]
-  x   <- init
-  n   <- nrow(x)
-  m   <- ncol(x)
-  wdist <- matrix(c(0,1,0,1,0,1,0,1,0), ncol=3)
-  trees  <- simecol::neighbors(init, state=1, wdist=wdist)
-  utrees <- simecol::neighbors(init, state=2, wdist=wdist)
-  t_fires <- simecol::neighbors(init, state=3, wdist=wdist)
-  u_fires <- simecol::neighbors(init, state=4, wdist=wdist)
-  rand_mat <- runif(init)
-  if(any(x>=3)){
+  while((length(which(init==3))+length(which(init==4)))>0){
+    x   <- init
+    wdist <- matrix(c(0,1,0,1,0,1,0,1,0), ncol=3)
+    trees  <- simecol::neighbors(init, state=1, wdist=wdist)
+    utrees <- simecol::neighbors(init, state=2, wdist=wdist)
+    rand_mat <- runif(init)
+    
     ## burning rules
     x[which(init==1 & t_fires>0 & rand_mat < (1-p_treeburn^t_fires))] <- 3
     x[which(init==2 & (t_fires>0 | u_fires>0) & rand_mat < p_uburn)] <- 4
@@ -28,13 +63,36 @@ fire_sim  <- function(init, params) {
     x[which(init==4 & rand_mat < p_burnout_u)] <- 1
     x[which(init==4 & rand_mat >= (p_burnout_u+p_stayburn_u))] <- 3
     x[which(init==3& rand_mat <p_burnout_t) ] <- 0
-  }else{
+    
+    init <- x
+  }
+  return(x)
+}
+
+growth_sim <- function(map, params, n=1){
+  p_treeburn <- params[1]
+  p_uburn <- params[2]
+  p_burnout_u <- params[3]
+  p_stayburn_u <- params[4]
+  p_burnout_t <- params[5]
+  p_seed <- params[6]
+  p_spread <- params[7]
+  p_grow <- params[8]
+  for(i in 1:n){
+    x   <- init
+    wdist <- matrix(c(0,1,0,1,0,1,0,1,0), ncol=3)
+    trees  <- simecol::neighbors(init, state=1, wdist=wdist)
+    utrees <- simecol::neighbors(init, state=2, wdist=wdist)
+    t_fires <- simecol::neighbors(init, state=3, wdist=wdist)
+    u_fires <- simecol::neighbors(init, state=4, wdist=wdist)
+    rand_mat <- runif(init)
     ## lightning rule
     x[which(init==2 & rand_mat<(3/n^2))] <- 4
     ## growth of trees and understory growth
     x[which(init==0 & rand_mat < p_seed)] <- 1
     x[which(init==1 & rand_mat < p_grow)] <- 2
     x[which(init==0 & (trees>0 |utrees>0) & rand_mat < p_spread*(trees+utrees))] <- 1
+    init <- x
   }
   return(x)
 }
