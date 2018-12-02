a63711cca3efeace8516279ec570d6f880508e77
Andrew Becker <andrewbecker93@gmail.com>
2018-12-02 12:07:21 -0500

    First full draft of controlled_burn_function.R

--- controlled_burn_function.R
+++ controlled_burn_function.R
@@ -15,7 +15,6 @@ controlled_burn <- function(map, budget, params){
   x   <- init
   n   <- nrow(x)
   m   <- ncol(x)
-  x <- init
   x_understory <- x
   x_understory[x_understory==1]<-0
   #build connected components
@@ -31,12 +30,22 @@ controlled_burn <- function(map, budget, params){
   #targets_2 <- targets[which(targets >= clump_limit_upper)]
   
   #clear trees around largest burn clumps, then ignite, burn, and subtract from budget
-  while(budget > 0){
+  wdist <- matrix(c(0,1,0,1,0,1,0,1,0), ncol=3)
+  while(budget > 0 & length(targets>0)){
     burn_target <- targets[1] #get clump to burn
     targets <- targets[-1] #remove clump from list
-    
+    removal_mat <- x
+    removal_mat[clump==as.integer(names(burn_target))] <- 5
+    neighbors <- simecol::neighbors(removal_mat, state=5, wdist=wdist)
+    trees_remove <- which(trees!=0)
+    actual_trees <- x[trees_remove]
+    trees_remove_true <- trees_remove[actual_trees==1]
+    cost <- length(trees_remove_true) + 5
+    if(cost <= budget){
+      x[trees_remove_true] <- 0
+      x[median(clump==as.integer(names(burn_target)))] <- 4
+      budget <- budget - cost
+    }
   }
-  
-  
-  
+  return(x, budget)
 }
